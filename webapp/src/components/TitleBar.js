0a85dfc0dfe2fec0e9ca4033eaf41451ac6490cb
Amir Al Nahawandi <amir.al@hm.edu>
2022-12-09 12:58:35 +0100

    components und header png

---
+++ webapp/src/components/TitleBar.js
@@ -0,0 +1,12 @@
+import logo from  "./images/welle_header.png" // anderer Speicherort
+
+export default function TitleBar() {
+    return (
+        <div className="titlebar">
+            <img src={logo} className="headerWelle" alt="welle" />
+        </div>
+    )
+}            
+
+//<link href='https://css.gg/add-r.css' rel='stylesheet'></link>
+//            <i class="gg-add-r"></i>
