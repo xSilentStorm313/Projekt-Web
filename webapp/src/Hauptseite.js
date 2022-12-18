a43eb0413ec23e63d5b6b468d2936ed0cfb5fdb4
Sophie Schmitz <sophie.schmitz@hm.edu>
2022-12-18 13:48:57 +0100

    Hauptseite bearbeitet

--- webapp/src/Hauptseite.js
+++ webapp/src/Hauptseite.js
@@ -1,12 +1,59 @@
 import React from "react";
 import "./Hauptseite.css"
+import TitleBarHauptseite from './components/TitleBarHauptseite';
 
-function Hauptseite() {
+import styled from 'styled-components';
 
+const Header = styled.div` // 1
+  height: 100px;
+  width: 100%;
+  background: #fff;
+  display: flex; // 2
+  justify-content: center; 
+`;
+
+const Container = styled.div`
+  background: #fff;
+  display: flex;
+  justify-content: center; // 1
+  flex-flow: column wrap; // 2
+  width: 100%;
+  height: 100%;
+  flex-flow: row wrap;
+`;
+const List = styled.div`
+  display: flex;
+  justify-content: center; // 3
+  flex-flow: row wrap; // 4
+  flex-flow: column wrap;
+`;
+
+const Card = styled.div`
+  margin: 20px;
+  background: rgb(132, 91, 162);
+  height: 320px;
+  width: 320px;
+  border-radius: 20px;
+  box-shadow: 2px 2px 5px 5px rgb(210, 210, 210);
+  display: flex;
+  flex-flow: column; // 5 
+  justify-content: center;
+  align-items: center;
+`;
+
+function App() {
     return (
-        <div className="my-page">
-        </div>
-    )
+        <Container>
+            <TitleBarHauptseite/>
+            <Header> 
+                <p>Suchfeld</p>
+            </Header>
+            <List>
+                {[0, 1, 2, 3, 5, 6, 7, 8, 9].map(item => <Card>{item}</Card>)}
+            </List>
+        </Container>
+    );
 }
 
-export default Hauptseite;
+export default App;
+
---
+++ webapp/src/components/TitleBarHauptseite.js
@@ -0,0 +1,14 @@
+import header from  "./images/welle_header_lila.png" // anderer Speicherort
+import React from "react"
+
+export default function TitleBarHauptseite() {
+    return (
+        <div className="titlebarhauptseite">
+            <img src={header} className="headerWelle" alt="lilawelle" />
+            <br/><br/>
+        </div>
+    )
+}            
+
+//<link href='https://css.gg/add-r.css' rel='stylesheet'></link>
+//            <i class="gg-add-r"></i>
