c745829810f18065de1b7a673cdfb5ce8eaa8caa
Katharina Grünwald <kgruenwa@hm.edu>
2022-12-21 17:47:25 +0100

    hauptseite Arbeitserlaubnis

--- webapp/src/components/Beiträge.js
+++ webapp/src/components/Beiträge.js
@@ -1,18 +1,18 @@
 import React from "react";
 import formular from "./images/kindergeldantraghalb.png";
+import arbeitserlaubnis from "./images/arbeitserlaubnis.svg"
 
 export default function Button() {
     return (
         <div>
+          <br className="abstand"/>
           <a href="/beitrag"><span className="buttonbeitrag">
             <img src={formular} className="formular" alt="formular" /> Kindergeld <p className="beschreibung">Frage zu Absatz 3</p> </span></a>
-          <br/>
-          <a href="/beitrag"><span className="buttonbeitrag">zwei</span></a>
-          <br/>
+          <a href="/beitrag"><span className="buttonbeitrag">
+            <img src={arbeitserlaubnis} className="formular" alt="formular"/> Arbeitserlaubnis</span></a>
           <a href="/beitrag"><span className="buttonbeitrag">drei</span></a>
-          <br/>
           <a href="/beitrag"><span className="buttonbeitrag">vier</span></a>
-          <br/>
+          
         </div>
     );
 }
