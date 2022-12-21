8d4ee79955af2a831172b81c870b1f0b5ff2ce46
Katharina Grünwald <kgruenwa@hm.edu>
2022-12-21 17:19:18 +0100

    Beiträge mit Bild

--- webapp/src/components/Beiträge.js
+++ webapp/src/components/Beiträge.js
@@ -1,9 +1,11 @@
 import React from "react";
+import formular from "./images/kindergeldantraghalb.png";
 
 export default function Button() {
     return (
         <div>
-          <a href="/beitrag"><span className="buttonbeitrag">eins</span></a>
+          <a href="/beitrag"><span className="buttonbeitrag">
+            <img src={formular} className="formular" alt="formular" /> Kindergeld <p className="beschreibung">Frage zu Absatz 3</p> </span></a>
           <br/>
           <a href="/beitrag"><span className="buttonbeitrag">zwei</span></a>
           <br/>
