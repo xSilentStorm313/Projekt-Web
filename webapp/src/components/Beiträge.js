56f99332162d3ef67099a1c5fb414146ea4fc530
Katharina Grünwald <kgruenwa@hm.edu>
2022-12-22 10:54:16 +0100

    Hauptseite einzelne Beitäge hinzugefügt

--- webapp/src/components/Beiträge.js
+++ webapp/src/components/Beiträge.js
@@ -1,6 +1,10 @@
 import React from "react";
 import formular from "./images/kindergeldantraghalb.png";
 import arbeitserlaubnis from "./images/arbeitserlaubnis.svg";
+import anerkennung from "/Users/katharinagrunwald/Projekt-Web-1/webapp/src/components/images/anerkennunngabschluesse.svg";
+import asylbeantragen from "/Users/katharinagrunwald/Projekt-Web-1/webapp/src/components/images/asylbeantragen.svg";
+import sportverein from "/Users/katharinagrunwald/Projekt-Web-1/webapp/src/components/images/sportverein.svg";
+import wohnen from "/Users/katharinagrunwald/Projekt-Web-1/webapp/src/components/images/wohnen.svg";
 
 export default function Button() {
     return (
@@ -12,9 +16,17 @@ export default function Button() {
           <a href="/arbeitserlaubnis"><span className="buttonbeitrag">
             <img src={arbeitserlaubnis} className="formular" alt="formular"/>Arbeitserlaubnis</span></a>
 
-          <a href="/beitrag"><span className="buttonbeitrag">drei</span></a>
-          <a href="/beitrag"><span className="buttonbeitrag">vier</span></a>
+          <a href="/beitrag"><span className="buttonbeitrag">
+            <img src={anerkennung} className="formular" alt="formular"/>Anerkennung der Abschlüsse</span></a>
+
+          <a href="/beitrag"><span className="buttonbeitrag">
+            <img src={asylbeantragen} className="formular" alt="formular"/>Asyl beantragen</span></a>
+
+          <a href="/beitrag"><span className="buttonbeitrag">
+            <img src={sportverein} className="formular" alt="formular"/>Vereinsanmeldung</span></a>
           
+          <a href="/beitrag"><span className="buttonbeitrag">
+            <img src={wohnen} className="formular" alt="formular"/>Beantragen Wohngeld</span></a>
         </div>
     );
 }
