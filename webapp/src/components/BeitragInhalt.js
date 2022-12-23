bd6ad7c5ad3939e3097b00599b33a96a66322aa1
Katharina Grünwald <kgruenwa@hm.edu>
2022-12-23 12:44:15 +0100

    Komments bei Kindergeld hinzugefügt

--- webapp/src/components/BeitragInhalt.js
+++ webapp/src/components/BeitragInhalt.js
@@ -7,10 +7,14 @@ export default function Inhalt() {
         <div>
             <h1 className="überschrift">Kindergeld</h1>
             <img src={formular} className="formular" alt="formular" />
+        
                 
             <a href="/beitrag">
                 <span className="fragebeitrag">Ich hätte eine Frage zu Absatz 3 woher kriege ich folgende Informationen?</span>
                 </a>
+            <p className="komment">Du musst folgende Informationen aus deinen persönlichen Blättern welche dir die AOK zugesendet haben muss entnehmen</p>
+            <p className="antwortkomment">Die kannst du auch anfordern  bei der AOK, falls du deine  nicht mehr findest</p>
+            <p className="komment">Dazu liegt im folgenden Aritkel noch was vor: kindergeld</p>
         </div>
     )
 }
