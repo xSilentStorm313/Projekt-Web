a078b9a82740dc1d0f62e8e77046272219f75822
Katharina Grünwald <kgruenwa@hm.edu>
2022-12-23 12:53:36 +0100

    button antworten

--- webapp/src/components/BeitragInhalt.js
+++ webapp/src/components/BeitragInhalt.js
@@ -12,9 +12,9 @@ export default function Inhalt() {
             <a href="/beitrag">
                 <span className="fragebeitrag">Ich hätte eine Frage zu Absatz 3 woher kriege ich folgende Informationen?</span>
                 </a>
-            <p className="komment">Du musst folgende Informationen aus deinen persönlichen Blättern welche dir die AOK zugesendet haben muss entnehmen</p>
-            <p className="antwortkomment">Die kannst du auch anfordern  bei der AOK, falls du deine  nicht mehr findest</p>
-            <p className="komment">Dazu liegt im folgenden Aritkel noch was vor: kindergeld</p>
+            <p className="komment">Du musst folgende Informationen aus deinen persönlichen Blättern welche dir die AOK zugesendet haben muss entnehmen<button className="buttonantworten">Antworten</button></p>
+            <p className="antwortkomment">Die kannst du auch anfordern  bei der AOK, falls du deine  nicht mehr findest<button className="buttonantworten">Antworten</button></p>
+            <p className="komment">Dazu liegt im folgenden Aritkel noch was vor: kindergeld<button className="buttonantworten">Antworten</button></p>
         </div>
     )
 }
