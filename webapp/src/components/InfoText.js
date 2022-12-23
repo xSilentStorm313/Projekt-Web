ebe71592aa07b63109fed1cfc29b7d55991751e0
Katharina Grünwald <kgruenwa@hm.edu>
2022-12-23 12:27:02 +0100

    icon margin left hinzugefügt und leertasten delet

--- webapp/src/components/InfoText.js
+++ webapp/src/components/InfoText.js
@@ -9,11 +9,11 @@ export default function InfoText() {
     return (
         <div>
             <h4>Das kann dir Helping Hands alles bieten:</h4>
-            <p><img src={icon1} className="icon" alt="icon" align="left" /><p className="texthome"> &nbsp; &nbsp;Kostenlose Hilfe für bürokratische Angelegenheiten rund um die Uhr</p></p>
-            <p><img src={icon2} className="icon" alt="icon" align="left" /><p className="texthome"> &nbsp; &nbsp;Informationen zu neuen Gesetzen und Nachrichten mit Relevanz für Geflüchtete</p></p>
-            <p><img src={icon3} className="icon" alt="icon" align="left" /><p className="texthome"> &nbsp; &nbsp;Richtige Behördenwege finden</p></p>
-            <p><img src={icon4} className="icon" alt="icon" align="left" /><p className="texthome"> &nbsp; &nbsp;Formulare verstehen</p></p>
-            <p><img src={icon5} className="icon" alt="icon" align="left" /><p className="texthome"> &nbsp; &nbsp;Mit anderen Personen interagieren & sich gegenseitig weiterhelfen</p></p>
+            <p><img src={icon1} className="icon" alt="icon" align="left" /><p className="texthome"> Kostenlose Hilfe für bürokratische Angelegenheiten rund um die Uhr</p></p>
+            <p><img src={icon2} className="icon" alt="icon" align="left" /><p className="texthome"> Informationen zu neuen Gesetzen und Nachrichten mit Relevanz für Geflüchtete</p></p>
+            <p><img src={icon3} className="icon" alt="icon" align="left" /><p className="texthome"> Richtige Behördenwege finden</p></p>
+            <p><img src={icon4} className="icon" alt="icon" align="left" /><p className="texthome"> Formulare verstehen</p></p>
+            <p><img src={icon5} className="icon" alt="icon" align="left" /><p className="texthome"> Mit anderen Personen interagieren & sich gegenseitig weiterhelfen</p></p>
         </div>
     )
 }
