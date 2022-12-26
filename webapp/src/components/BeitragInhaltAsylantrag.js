457748d78cb25bf50f5f4680668cd90df8bf5672
Katharina Grünwald <kgruenwa@hm.edu>
2022-12-26 15:41:16 +0100

    box asyl antrag

--- webapp/src/components/BeitragInhaltAsylantrag.js
+++ webapp/src/components/BeitragInhaltAsylantrag.js
@@ -10,9 +10,9 @@ export default function BeitragInhaltAsylantrag() {
             <img src={asylbeantragen} className="formular" alt="formular" />
             <h1 className="überschrift1">Asyl beantragen</h1>
             <p className="text">Sie sind in Deutschland, um Asyl zu beantragen.</p>
-            <span className="infotextasyl">Grundsätzlich haben Sie das Recht auf ein Asylverfahren. Sie haben aber auch die Pflicht, während des gesamten Verfah­rens aktiv mitzuwirken und immer die Wahrheit zu sagen. Das ist wichtig für Sie, denn sonst gefährden Sie Ihr Asylverfahren.</span>
+            <div className="infotextasyl">Grundsätzlich haben Sie das Recht auf ein Asylverfahren. Sie haben aber auch die Pflicht, während des gesamten Verfah­rens aktiv mitzuwirken und immer die Wahrheit zu sagen. Das ist wichtig für Sie, denn sonst gefährden Sie Ihr Asylverfahren.</div>
             <h2 className="zwischenüberschrift">1.Regestrierung</h2>
-            <p className="text">Bei Ihrer Ankunft in Deutschland werden Sie zunächst regis­triert.
+            <p className="text">Bei Ihrer Ankunft in Deutschland werden Sie zunächst regis­triert.s
                 Wir fragen Sie nach Ihrem Namen, Herkunftsland, Geburtsdatum, Ihrer Religion, Sprache und Volkszugehörig­keit. Wir fotografieren Sie und nehmen Ihre Fingerabdrücke ab. Nach der Registrierung erhalten Sie einen An­kunftsnachweis und stellen direkt im Ankunftszentrum Ihren Asylantrag.
                 Es kann aber sein, dass Sie erst in einer Aufnahme­einrichtung untergebracht werden. Dort erhalten Sie Ihren Ankunftsnachweis und erfahren, wann Sie Ihren Asylantrag persönlich stellen können.
             </p>
