a61e3cda4a64c885c67417315a21e623d9882f8d
Katharina Grünwald <kgruenwa@hm.edu>
2022-12-26 11:36:07 +0100

    button zurück in einzelner beitrag

--- webapp/src/EinzelnerBeitrag.js
+++ webapp/src/EinzelnerBeitrag.js
@@ -4,11 +4,16 @@ import TitleBarHauptseite from './components/TitleBarHauptseite';
 import MenuIcon from "./components/MenuIcon";
 import BeitragInhalt from "./components/BeitragInhalt";
 import Kommentare from "./components/Kommentare";
+import ZurueckIcon from "./components/ZurückButton";
+import { Link } from "react-router-dom";
 
 export default function EinzelnerBeitrag() {
     return (
         <div className="hauptseite">
             <MenuIcon items={['Home', 'Account', 'Kategorien', 'Anfrage stellen', 'Meine Chats']} />
+            <Link to='/start'>
+                <ZurueckIcon/>
+            </Link>
             <TitleBarHauptseite />
             <BeitragInhalt />
             <Kommentare className="kommentar"/>
--- webapp/src/components/ZurückButton.js
+++ webapp/src/components/ZurückButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React  from 'react';
 import { AiOutlineLeft } from "react-icons/ai";
 
 
