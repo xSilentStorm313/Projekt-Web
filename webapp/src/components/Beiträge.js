7a512cc93afaac0dddd517a6ba665b1525863359
Katharina Grünwald <kgruenwa@hm.edu>
2022-12-26 10:37:28 +0100

    beschreiben hinzugefügt; Schriftgrößen angepasst

--- webapp/src/components/Beiträge.js
+++ webapp/src/components/Beiträge.js
@@ -7,26 +7,27 @@ import sportverein from "./images/sportverein.svg";
 import wohnen from "./images/wohnen.svg";
 
 export default function Button() {
-    return (
-        <div>
-          <br className="abstand"/>
-          <a href="/beitrag"><span className="buttonbeitrag">
-            <img src={formular} className="formular" alt="formular" /> Kindergeld <p className="beschreibung">Frage zu Absatz 3</p> </span></a>
+  return (
+    <div>
+      <br className="abstand" />
+      <a href="/beitrag"><span className="buttonbeitrag">
+        <img src={formular} className="formular" alt="formular"/> Kindergeld  <span className="beschreibung"> <br />Frage zu Absatz 3</span> </span></a>
 
-          <a href="/arbeitserlaubnis"><span className="buttonbeitrag">
-            <img src={arbeitserlaubnis} className="formular" alt="formular"/>Arbeitserlaubnis</span></a>
+      <a href="/arbeitserlaubnis"><span className="buttonbeitrag">
+        <img src={arbeitserlaubnis} className="formular" alt="formular"/>Arbeitserlaubnis <span className="beschreibung"> <br />In diesem Artikel erfährst du,
+          wie du als EU und non-EU Bürger in Deutschland arbeiten kannst</span> </span></a>
+ 
+      <a href="/beitrag"><span className="buttonbeitrag">
+        <img src={anerkennung} className="formular" alt="formular" />Anerkennung der Abschlüsse</span></a>
 
-          <a href="/beitrag"><span className="buttonbeitrag">
-            <img src={anerkennung} className="formular" alt="formular"/>Anerkennung der Abschlüsse</span></a>
+      <a href="/asylantrag"><span className="buttonbeitrag">
+        <img src={asylbeantragen} className="formular" alt="formular" />Asyl beantragen</span></a>
 
-          <a href="/asylantrag"><span className="buttonbeitrag">
-            <img src={asylbeantragen} className="formular" alt="formular"/>Asyl beantragen</span></a>
+      <a href="/beitrag"><span className="buttonbeitrag">
+        <img src={sportverein} className="formular" alt="formular" />Vereinsanmeldung</span></a>
 
-          <a href="/beitrag"><span className="buttonbeitrag">
-            <img src={sportverein} className="formular" alt="formular"/>Vereinsanmeldung</span></a>
-          
-          <a href="/beitrag"><span className="buttonbeitrag">
-            <img src={wohnen} className="formular" alt="formular"/>Beantragen Wohngeld</span></a>
-        </div>
-    );
+      <a href="/beitrag"><span className="buttonbeitrag">
+        <img src={wohnen} className="formular" alt="formular" />Beantragen Wohngeld</span></a>
+    </div>
+  );
 }
