80738b9316a75a36d9f7fc59a5bec9b5abd8e535
Katharina Grünwald <kgruenwa@hm.edu>
2022-12-26 11:38:54 +0100

    zurück button überall eingefügt funktioniert

--- webapp/src/Arbeitserlaubnis.js
+++ webapp/src/Arbeitserlaubnis.js
@@ -4,11 +4,16 @@ import TitleBarHauptseite from './components/TitleBarHauptseite';
 import MenuIcon from "./components/MenuIcon";
 import BeitragInhaltArbeitserlaubnis from "./components/BeitragInhaltArbeitserlaubnis";
 import Kommentare from "./components/Kommentare";
+import ZurueckIcon from "./components/ZurückButton";
+import { Link } from "react-router-dom";
 
 export default function Arbeitserlaubnis() {
     return (
         <div className="hauptseite">
             <MenuIcon items={['Home', 'Account', 'Kategorien', 'Anfrage stellen', 'Meine Chats']} />
+            <Link to='/start'>
+                <ZurueckIcon/>
+            </Link>
             <TitleBarHauptseite />
             <BeitragInhaltArbeitserlaubnis />
             <Kommentare className="kommentar"/>
--- webapp/src/Asylantrag.js
+++ webapp/src/Asylantrag.js
@@ -4,11 +4,16 @@ import TitleBarHauptseite from './components/TitleBarHauptseite';
 import MenuIcon from "./components/MenuIcon";
 import BeitragInhaltAsylantrag from "./components/BeitragInhaltAsylantrag";
 import Kommentare from "./components/Kommentare";
+import ZurueckIcon from "./components/ZurückButton";
+import { Link } from "react-router-dom";
 
 export default function Asylantrag() {
     return (
         <div className="hauptseite">
             <MenuIcon items={['Home', 'Account', 'Kategorien', 'Anfrage stellen', 'Meine Chats']} />
+            <Link to='/start'>
+                <ZurueckIcon/>
+            </Link>
             <TitleBarHauptseite />
             <BeitragInhaltAsylantrag />
             <Kommentare className="kommentar"/>
