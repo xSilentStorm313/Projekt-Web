9fd8992e3ed8a437b41094f64d2af97cfa5e51fc
Katharina Grünwald <kgruenwa@hm.edu>
2022-12-27 10:48:05 +0100

    asylantrag veränderungen

--- webapp/src/components/BeitragInhaltAsylantrag.js
+++ webapp/src/components/BeitragInhaltAsylantrag.js
@@ -15,22 +15,22 @@ export default function BeitragInhaltAsylantrag() {
             <div className="infotextasyl">Grundsätzlich haben Sie das Recht auf ein Asylverfahren. Sie haben aber auch die Pflicht, während des gesamten Verfah­rens aktiv mitzuwirken und immer die Wahrheit zu sagen. Das ist wichtig für Sie, denn sonst gefährden Sie Ihr Asylverfahren.</div>
             <h2 className="zwischenüberschrift">1.Regestrierung</h2>
             <p className="text">Bei Ihrer Ankunft in Deutschland werden Sie zunächst regis­triert.s
-                Wir fragen Sie nach Ihrem Namen, Herkunftsland, Geburtsdatum, Ihrer Religion, Sprache und Volkszugehörig­keit. Wir fotografieren Sie und nehmen Ihre Fingerabdrücke ab. Nach der Registrierung erhalten Sie einen An­kunftsnachweis und stellen direkt im Ankunftszentrum Ihren Asylantrag.
+                Wir fragen Sie nach Ihrem Namen, Herkunftsland, Geburtsdatum, Ihrer Religion, Sprache und Volkszugehörig­keit. Wir fotografieren Sie und nehmen Ihre Fingerab- drücke ab. Nach der Registrierung erhalten Sie einen An­kunftsnachweis und stellen direkt im Ankunftszentrum Ihren Asylantrag.
                 Es kann aber sein, dass Sie erst in einer Aufnahme­einrichtung untergebracht werden. Dort erhalten Sie Ihren Ankunftsnachweis und erfahren, wann Sie Ihren Asylantrag persönlich stellen können.
             </p>
             <a href="/arbeitserlaubnis"></a>
             <br/>
             <p className="komment">Was ist wenn ich meinen Geburtsurkunde verloren habe? Ist das schlimm? Kann ich die irgendwo neu beantragen?
             <br/>
-            <MdAccountCircle size={30} />
+            <MdAccountCircle size={30} className="Profilbild antwort"/>
             <button className="buttonantworten">Antworten</button></p>
             <p className="antwortkomment">Was ist wenn ich meinen Geburts- urkunde verloren habe? Ist das schlimm?Kann ich die irgendwo neu beantragen?
             <br/>
-            <MdAccountCircle size={30} />
+            <MdAccountCircle size={30} className="Profilbild antwort"/>
             <button className="buttonantworten">Antworten</button></p>
             <p className="komment">Was ist wenn ich den Termin für die Anhörung verpasse?
             <br/>
-            <MdAccountCircle size={30} />
+            <MdAccountCircle size={30} className="Profilbild antwort"/>
             <button className="buttonantworten">Antworten</button></p>
         </div>
     );
