e327961f693e0e9672ce9370b5357fcfa9a08d3d
Timo Warendorf <twarendo@hm.edu>
2022-12-27 15:09:40 +0100

    SwitchButton angepasst und CSS Änderungen

--- webapp/src/components/SwitchButton.js
+++ webapp/src/components/SwitchButton.js
@@ -4,6 +4,11 @@ import './Switch.css';
 const Switch = () => {
   return (
     <>
+      <h1 className="text-left-right">
+        <span className="left-text">Hilfesuchend</span>
+        <span className="byline">Helfer</span>
+      </h1>
+
       <input
         className="react-switch-checkbox"
         id={`react-switch-new`}
