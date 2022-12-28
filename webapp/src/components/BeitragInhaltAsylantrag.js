1e242161e91f54093126044ac009172d28351d08
Katharina Grünwald <kgruenwa@hm.edu>
2022-12-28 12:55:53 +0100

    Asylverfahren artikel weitergearbeitet

--- webapp/src/components/BeitragInhaltAsylantrag.js
+++ webapp/src/components/BeitragInhaltAsylantrag.js
@@ -10,15 +10,22 @@ export default function BeitragInhaltAsylantrag() {
         <div>
             <br className="abstand" />
             <img src={asylbeantragen} className="formular" alt="formular" />
-            <h1 className="überschrift1">Asyl beantragen</h1>
+            <button className="downloadbutton">Download Formulare</button>
+            <h1 className="überschrift1">Asylverfahren</h1>
             <p className="text">Sie sind in Deutschland, um Asyl zu beantragen.</p>
             <div className="infotextasyl">Grundsätzlich haben Sie das Recht auf ein Asylverfahren. Sie haben aber auch die Pflicht, während des gesamten Verfah­rens aktiv mitzuwirken und immer die Wahrheit zu sagen. Das ist wichtig für Sie, denn sonst gefährden Sie Ihr Asylverfahren.</div>
             <h2 className="zwischenüberschrift">1.Registrierung</h2>
-            <p className="text">Bei Ihrer Ankunft in Deutschland werden Sie zunächst regis­triert.s
-                Wir fragen Sie nach Ihrem Namen, Herkunftsland, Geburtsdatum, Ihrer Religion, Sprache und Volkszugehörig­keit. Wir fotografieren Sie und nehmen Ihre Fingerab- drücke ab. Nach der Registrierung erhalten Sie einen An­kunftsnachweis und stellen direkt im Ankunftszentrum Ihren Asylantrag.
+            <p className="text">Bei Ihrer Ankunft in Deutschland werden Sie zunächst regis­triert <br/> <br/>
+                Wir fragen Sie nach Ihrem Namen, Herkunftsland, Geburtsdatum, Ihrer Religion, Sprache und Volkszugehörig­keit. Wir fotografieren Sie und nehmen Ihre Fingerab- drücke ab. Nach der Registrierung erhalten Sie einen An­kunftsnachweis und stellen direkt im Ankunftszentrum Ihren Asylantrag.<br/><br/>
                 Es kann aber sein, dass Sie erst in einer Aufnahme­einrichtung untergebracht werden. Dort erhalten Sie Ihren Ankunftsnachweis und erfahren, wann Sie Ihren Asylantrag persönlich stellen können.
             </p>
-            <a href="/arbeitserlaubnis"></a>
+            <h2 className="zwischenüberschrift">2. Persönliche Antragsstellung</h2>
+            <h2 className="zwischenüberschrift">3. Dublin-Verfahren</h2>
+            <h2 className="zwischenüberschrift">4. Persönliche Anhörung</h2>
+            <h2 className="zwischenüberschrift">5. Ausgang Asylverfahren</h2>
+
+            <p className="quellen">Quellen</p>
+
             <br/>
             <p className="komment">Was ist wenn ich meinen Geburtsurkunde verloren habe? Ist das schlimm? Kann ich die irgendwo neu beantragen?
             <br/>
