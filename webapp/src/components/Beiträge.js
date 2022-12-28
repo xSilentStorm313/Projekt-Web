56ce02eb0155f9b83979837c82636a81b6c8109b
Katharina Grünwald <kgruenwa@hm.edu>
2022-12-28 12:30:26 +0100

    Hauptseite angepasst

--- webapp/src/components/Beiträge.js
+++ webapp/src/components/Beiträge.js
@@ -10,24 +10,26 @@ export default function Button() {
   return (
     <div>
       <br className="abstand" />
-      <a href="/beitrag"><span className="buttonbeitrag">
-        <img src={formular} className="formular" alt="formular"/> Kindergeld  <span className="beschreibung"> <br />Frage zu Absatz 3</span> </span></a>
 
       <a href="/arbeitserlaubnis"><span className="buttonbeitrag">
         <img src={arbeitserlaubnis} className="formular" alt="formular"/>Arbeitserlaubnis <span className="beschreibung"> <br />In diesem Artikel erfährst du,
           wie du als EU und non-EU Bürger in Deutschland arbeiten kannst</span> </span></a>
  
       <a href="/beitrag"><span className="buttonbeitrag">
-        <img src={anerkennung} className="formular" alt="formular" />Anerkennung der Abschlüsse</span></a>
+        <img src={anerkennung} className="formular" alt="formular" />Anerkennung der Abschlüsse<span className="beschreibung"> <br />Hier wird dir gezeigt, wie du welche Abschlüsse anerkennen lassen kannst</span></span></a>
 
       <a href="/asylantrag"><span className="buttonbeitrag">
-        <img src={asylbeantragen} className="formular" alt="formular" />Asyl beantragen</span></a>
+        <img src={asylbeantragen} className="formular" alt="formular" />Asyl beantragen
+        <span className="beschreibung"> <br />Hier erhälst du Hilfe für dein Asylverfahren</span> </span> </a>
 
       <a href="/beitrag"><span className="buttonbeitrag">
-        <img src={sportverein} className="formular" alt="formular" />Vereinsanmeldung</span></a>
+        <img src={sportverein} className="formular" alt="formular" />Vereinsanmeldung<span className="beschreibung"> <br />Hier findest Sport und Freizeitangebote</span></span></a>
 
       <a href="/beitrag"><span className="buttonbeitrag">
-        <img src={wohnen} className="formular" alt="formular" />Beantragen Wohngeld</span></a>
+        <img src={wohnen} className="formular" alt="formular" />Beantragen Wohngeld <span className="beschreibung"> <br />Hier findest Informationen zum Thema Wohnen wie zum Beispiel zur Wohnungsanmeldung</span></span></a>
+      
+        <a href="/beitrag"><span className="buttonbeitrag">
+        <img src={formular} className="formular" alt="formular"/> Kindergeld  <span className="beschreibung"> <br />Frage zu Absatz 3</span> </span></a>
     </div>
   );
 }
