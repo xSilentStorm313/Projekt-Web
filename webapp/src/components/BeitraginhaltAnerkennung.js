618495fa9570feed6ad5d835a4c4f4945fd20bbe
Katharina Grünwald <kgruenwa@hm.edu>
2022-12-29 10:45:51 +0100

    Anerkennung Abschlüsse

---
+++ webapp/src/components/BeitraginhaltAnerkennung.js
@@ -0,0 +1,36 @@
+import React from "react";
+import anerkennung from "./images/anerkennunngabschluesse.svg";
+import { MdAccountCircle } from "react-icons/md";
+
+export default function BeitragInhaltAnerkennung() {
+
+    return (
+        <div>
+            <br className="abstand" />
+            <img src={anerkennung} className="formular" alt="formular" />
+            <button className="downloadbutton">Download Formulare</button>
+            <h1 className="überschrift1">Anerkennung der Abschlüsse</h1>
+            <p className="text">In Deutschland kommt es bei der Anerkennung der Abschlüsse sehr auf den jeweiligen Abschluss an.</p>
+            <div className="infotextasyl">Dementsprechend empfehlen wir Ihnen das <a href="https://www.anerkennung-in-deutschland.de/html/de/fachkraefte.php">Infoportal der Bundesregierung
+                zur Anerkennung ausländischer Berufsqualifikationen</a> anzuschauen.</div>
+            <p className="text">Geben sie in dies Suchleiste ihren Job ein, den sie in Ihrem Lang ausgeführt haben. Und das Portal führt sich durch die einzelnen Schritte die Sie brauchen um ihre Abschlüsse und Arbeitserfahrung in Deutschland anerkennen lassen zu können.</p>
+
+
+
+            <p className="quellen">Quellen</p>
+
+            <p className="komment">Was ist wenn ich meinen Geburtsurkunde verloren habe? Ist das schlimm? Kann ich die irgendwo neu beantragen?
+                <br />
+                <MdAccountCircle size={30} className="Profilbild antwort" />
+                <button className="buttonantworten">Antworten</button></p>
+            <p className="antwortkomment">Was ist wenn ich meinen Geburts- urkunde verloren habe? Ist das schlimm?Kann ich die irgendwo neu beantragen?
+                <br />
+                <MdAccountCircle size={30} className="Profilbild antwort" />
+                <button className="buttonantworten">Antworten</button></p>
+            <p className="komment">Was ist wenn ich den Termin für die Anhörung verpasse?
+                <br />
+                <MdAccountCircle size={30} className="Profilbild antwort" />
+                <button className="buttonantworten">Antworten</button></p>
+        </div>
+    );
+}
