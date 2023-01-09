a1b9e4d9039afefcec32e28ad1b472da3c693cf2
Katharina Grünwald <kgruenwa@hm.edu>
2023-01-09 16:33:56 +0100

    hauptseite grün änderung

--- webapp/src/components/BeiträgeHelfer.js
+++ webapp/src/components/BeiträgeHelfer.js
@@ -7,10 +7,10 @@ export default function Button() {
     <div className="abstand">
  
       <a href="/verkehr"><span className="buttonbeitraggrün">
-        <img src={verkehr} className="formular" alt="formular" />freiwillige Rückkehr <span className="beschreibung"> <br />Hier findest Informationen zum Thema freiwillige Rückkehr als Geflüchteter in das Ursprungsland</span></span></a>
+        <img src={verkehr} className="formular" alt="formular" />Verkehr <span className="beschreibung"> <br />Hier findest du Informationen zum Thema kostenfeier Verkehr mit den Verkehrs- mitteln für ukrainische Geflüchtete</span></span></a>
       
         <a href="/rückkehr"><span className="buttonbeitraggrün">
-        <img src={rückkehr} className="formular" alt="formular" />freiwillige Rückkehr <span className="beschreibung"> <br />Hier findest Informationen zum Thema freiwillige Rückkehr als Geflüchteter in das Ursprungsland</span></span></a>
+        <img src={rückkehr} className="formular" alt="formular" />freiwillige Rückkehr <span className="beschreibung"> <br />Hier findest du Informationen zum Thema freiwillige Rückkehr als Geflüchteter in das Ursprungsland</span></span></a>
 
     </div>
   );
