7fae10c26b1373977f20350c83c16400642ee136
Katharina Grünwald <kgruenwa@hm.edu>
2023-01-12 01:41:03 +0100

    eslint fehler behebung

--- webapp/src/components/Regestrierung/ButtonRegistrieren.js
+++ webapp/src/components/Regestrierung/ButtonRegistrieren.js
@@ -2,13 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function ButtonRegestrierung() {
-    return (
-        <div className="button">
-          <a>
-            <Link to="/start">
-            <input type="submit" value="JETZT REGISTRIEREN" className="registrierenweiterbutton"/>
-            </Link>
-
-          </a>
-        </div>
-    ) }
+  return (
+    <div className="button">
+      <Link to="/start">
+        <input type="submit" value="JETZT REGISTRIEREN" className="registrierenweiterbutton" />
+      </Link>
+    </div>
+  )
+}
