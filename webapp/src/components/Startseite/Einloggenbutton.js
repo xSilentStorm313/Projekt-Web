691adb565c8dae0d24e3f6ae0932c70ca5feecd5
Katharina Grünwald <kgruenwa@hm.edu>
2023-01-13 10:54:14 +0100

    startseite veränderung

--- webapp/src/components/Startseite/Einloggenbutton.js
+++ webapp/src/components/Startseite/Einloggenbutton.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 export default function Button() {
     return (
         <div className="buttonlogin">
-            <Link to="/login">EINLOGGEN</Link>
+            <Link to="/login">
+                <p className="buttonlogintext">EINLOGGEN</p>
+                </Link>
         </div>
     ) }
