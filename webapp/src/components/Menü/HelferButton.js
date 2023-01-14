654e08aade3f7957a892482230cf95c54b9471c2
Katharina Grünwald <kgruenwa@hm.edu>
2023-01-14 17:17:16 +0100

    kleinere Buttons menü

--- webapp/src/components/Menü/HelferButton.js
+++ webapp/src/components/Menü/HelferButton.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 export default function Button() {
   return (
-    <div className="button-switch-helfer">
+    <button className="button-switch-helfer">
       <Link to="/startgrün">
         <p className="button-switch-schrift">Helfer</p>
       </Link>
-    </div>
+    </button>
   )
 }
 
