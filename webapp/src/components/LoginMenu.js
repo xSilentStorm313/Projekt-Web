1c98ed7dc4d1753506c6f159240cecdf4c7fdc7c
Amir Al Nahawandi <amir.al@hm.edu>
2023-01-14 16:41:57 +0100

    Login mit backend verbunden

--- webapp/src/components/LoginMenu.js
+++ webapp/src/components/LoginMenu.js
@@ -1,37 +1,79 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function LoginMenu() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch("http://localhost:3001/users/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, password }),
+            });
+            const data = await response.json();
+            if (!response.ok) {
+                throw new Error(data.error);
+            }
+            // authenticate user and redirect
+            navigate('/start')
+        } catch (err) {
+            setError(err.message);
+        }
+    };
+
 
     return (
         <div className="login-menu">
             <h1 className="überschriftlogin">Einloggen</h1>
-            <form>
+            {error && <p>{error}</p>}
+            <form onSubmit={handleSubmit}>
                 <label>
                     <div className="textlogin">
                         Benutzername:
                     </div>
                     <br />
-                    <input type="text" name="username" className="input" placeholder="Benutzerename"/>
+                    <input
+                        type="text"
+                        name="email"
+                        className="input"
+                        placeholder="Benutzerename"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </label>
                 <br />
-                <label >
+                <label>
                     <div className="textlogin">
                         Passwort:
                     </div>
                     <br />
-                    <input type="password" name="password" className="input" placeholder="Passwort"/>
+                    <input
+                        type="password"
+                        name="password"
+                        className="input"
+                        placeholder="Passwort"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </label>
                 <br />
                 <div className="button">
-                    <Link to="/start">
-                        <input type="submit" value="EINLOGGEN" className="buttonlogin" />
-                    </Link>
+                    <input type="submit" value="EINLOGGEN" className="buttonlogin" />
                 </div>
                 <p className="passwortvergessen">Passwort vergessen</p>
                 <br />
                 <br />
-                <p>Du hast kein Konto? <Link to="/registration" className="registrierung">Jetzt Registrieren</Link></p>
+                <p>
+                    Du hast kein Konto?{" "}
+                    <Link to="/registration" className="registrierung">
+                        Jetzt Registrieren
+                    </Link>
+                </p>
             </form>
         </div>
     );
--- webapp/src/components/Regestrierung/RegistrierenText.js
+++ webapp/src/components/Regestrierung/RegistrierenText.js
@@ -17,35 +17,35 @@ const RegestrierungText = () => {
 
   console.log('After useState')
 
-    const handleChange = (e) => {
-        console.log('HEYYYYYYYYYYY');
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-        });
-    }
+  const handleChange = (e) => {
+    console.log('HEYYYYYYYYYYY');
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  }
 
-    const  handleSubmit = async () => {
-        console.log('HEYYYYYYYYYYY');
-        const options = {
-            method: 'POST',
-            body: JSON.stringify(formData),
-            headers: {
-                'Content-Type': 'application/json',
-                'Origin': '*',
-                'Accept-Encoding': 'gzip, deflate, br',
-                'Accept': '*/*'
-            }
-        }
-        fetch('http://localhost:3001/users', options)
-        // .then(res => res)
-        .then(data => {
-            console.log(data);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+  const handleSubmit = async () => {
+    console.log('HEYYYYYYYYYYY');
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json',
+        'Origin': '*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept': '*/*'
+      }
     }
+    fetch('http://localhost:3001/users', options)
+      // .then(res => res)
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
   return (
     <div>
@@ -53,102 +53,102 @@ const RegestrierungText = () => {
       <form onSubmit={handleSubmit}></form>
 
       <p className="registrierungsbeschreibung">Name</p>
-        <input
-          type="text"
-          name="name"
-          className="input"
-          placeholder="Name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-        <p className="registrierungsbeschreibung">Vorname</p>
-        <input
-          type="text"
-          name="vorname"
-          className="input"
-          placeholder="Vorname"
-          value={formData.vorname}
-          onChange={handleChange}
-        />
-        <p className="registrierungsbeschreibung">Benutzername</p>
-        <input
-          type="text"
-          name="benutzername"
-          className="input"
-          placeholder="Benutzername"
-          value={formData.benutzername}
-          onChange={handleChange}
-        />
-        <p className="registrierungsbeschreibung">E-Mail</p>
-        <input
-          type="text"
-          name="email"
-          className="input"
-          placeholder="E-Mail"
-          value={formData.email}
-          onChange={handleChange}
-        />
-        <p className="registrierungsbeschreibung">Geburtsdatum</p>
-        <input
-          type="text"
-          name="birthdate"
-          className="input"
-          placeholder="Geburtsdatum"
-          value={formData.birthdate}
-          onChange={handleChange}
-        />
-        <p className="registrierungsbeschreibung">Passwort</p>
-        <input
-          type="password"
-          name="password"
-          className="input"
-          placeholder="Passwort"
-          value={formData.password}
-          onChange={handleChange}
-        />
-        <p className="registrierungsbeschreibung">Wohnsituation</p>
-        <select
-          name="wohnsituation"
-          id="wohnsituation-select"
-          className="auswaehleninput"
-          value={formData.wohnsituation}
-          onChange={handleChange}
-          >
-            <option value="">Bitte auswählen</option>
-            <option value="individuelles Wohnen">individuelles Wohnen</option>
-            <option value="gemeinschaftliches Wohnen">gemeinschaftliches Wohnen</option>
-            <option value="gemeinschaftliches Wohnen und Arbeiten">gemeinschaftliches Wohnen und Arbeiten</option>
-            <option value="institutionelles Wohnen">institutionelles Wohnen</option>
-        </select>
+      <input
+        type="text"
+        name="name"
+        className="input"
+        placeholder="Name"
+        value={formData.name}
+        onChange={handleChange}
+        required
+      />
+      <p className="registrierungsbeschreibung">Vorname</p>
+      <input
+        type="text"
+        name="vorname"
+        className="input"
+        placeholder="Vorname"
+        value={formData.vorname}
+        onChange={handleChange}
+      />
+      <p className="registrierungsbeschreibung">Benutzername</p>
+      <input
+        type="text"
+        name="benutzername"
+        className="input"
+        placeholder="Benutzername"
+        value={formData.benutzername}
+        onChange={handleChange}
+      />
+      <p className="registrierungsbeschreibung">E-Mail</p>
+      <input
+        type="text"
+        name="email"
+        className="input"
+        placeholder="E-Mail"
+        value={formData.email}
+        onChange={handleChange}
+      />
+      <p className="registrierungsbeschreibung">Geburtsdatum</p>
+      <input
+        type="text"
+        name="birthdate"
+        className="input"
+        placeholder="Geburtsdatum"
+        value={formData.birthdate}
+        onChange={handleChange}
+      />
+      <p className="registrierungsbeschreibung">Passwort</p>
+      <input
+        type="password"
+        name="password"
+        className="input"
+        placeholder="Passwort"
+        value={formData.password}
+        onChange={handleChange}
+      />
+      <p className="registrierungsbeschreibung">Wohnsituation</p>
+      <select
+        name="wohnsituation"
+        id="wohnsituation-select"
+        className="auswaehleninput"
+        value={formData.wohnsituation}
+        onChange={handleChange}
+      >
+        <option value="">Bitte auswählen</option>
+        <option value="individuelles Wohnen">individuelles Wohnen</option>
+        <option value="gemeinschaftliches Wohnen">gemeinschaftliches Wohnen</option>
+        <option value="gemeinschaftliches Wohnen und Arbeiten">gemeinschaftliches Wohnen und Arbeiten</option>
+        <option value="institutionelles Wohnen">institutionelles Wohnen</option>
+      </select>
 
-        <p className="registrierungsbeschreibung">Kinder</p>
-        <select
-          name="children"
-          id="kinder-select"
-          className="auswaehleninput"
-          value={formData.children}
-          onChange={handleChange}
-         >
-           <option value="">Bitte auswählen</option>
-           <option value="0">0</option>
-           <option value="1">1</option>
-           <option value="2">2</option>
-           <option value="3">3</option>
-           <option value="3">4</option>
-           <option value="3">5</option>
-           <option value="3">6</option>
-           <option value="3">Mehr als 6</option>
-        </select>
+      <p className="registrierungsbeschreibung">Kinder</p>
+      <select
+        name="children"
+        id="kinder-select"
+        className="auswaehleninput"
+        value={formData.children}
+        onChange={handleChange}
+      >
+        <option value="">Bitte auswählen</option>
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="3">4</option>
+        <option value="3">5</option>
+        <option value="3">6</option>
+        <option value="3">Mehr als 6</option>
+      </select>
 
-        <p className="registrierungsbeschreibung">Herkunftsland</p>
-        <select
-          name="country_of_origin"
-          id="herkunftsland-select"
-          className="auswaehleninput"
-          value={formData.country_of_origin}
-          onChange={handleChange}
-        >
+      <p className="registrierungsbeschreibung">Herkunftsland</p>
+      <select
+        name="country_of_origin"
+        id="herkunftsland-select"
+        className="auswaehleninput"
+        value={formData.country_of_origin}
+        onChange={handleChange}
+      >
         <option value="">Bitte auswählen</option>
         <option value="deutschland">Deutschland</option>
         <option value="griechenland">Griechenland</option>
@@ -166,24 +166,23 @@ const RegestrierungText = () => {
         <option value="afganistan">Afganistan</option>
         <option value="kenia">Kenia</option>
         <option value="peru">Peru</option>
-        </select>
-        <br/>
-        <br/>
+      </select>
+      <br />
+      <br />
 
-        <div className="button">
-            <Link to="/start">
-            <input type="submit" value="JETZT REGISTRIEREN" className="registrierenweiterbutton" onClick={handleSubmit} />
-            </Link>
-        </div>
+      <div className="button">
+        <Link to="/start">
+          <input type="submit" value="JETZT REGISTRIEREN" className="registrierenweiterbutton" onClick={handleSubmit} />
+        </Link>
+      </div>
 
-        </div>
+    </div>
 
 
-        
-    )
+
+  )
 }
 
 
 
 export { RegestrierungText }
- 
