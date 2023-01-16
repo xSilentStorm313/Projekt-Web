b8e36357719650e58d45638fa91b8884052ceb62
Timo Warendorf <twarendo@hm.edu>
2023-01-16 16:40:37 +0100

    logo responsive

--- webapp/src/components/Footer.js
+++ webapp/src/components/Footer.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 export default function Footer() {
     return (
-        <div className="footer">
+        <div>
             <a href="https://www.hm.edu/impressum/index.de.html" className="impressumlink">Impressum</a>&nbsp;&nbsp;&nbsp;&nbsp;
             <a href="https://www.hm.edu/datenschutz/datenschutzhinweis_studierende.de.html"className="überunslink">Über uns</a>
         </div>
