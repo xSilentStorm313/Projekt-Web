e6f23821556e07176144323ff55980b908db3925
Katharina Grünwald <kgruenwa@hm.edu>
2023-01-17 22:29:21 +0100

    not found bearbeitet zurück button und menü

--- webapp/src/NotFound.js
+++ webapp/src/NotFound.js
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom";
 import TitleBar from './components/Startseite/TitleBarStartseite';
+import MenuIcon from "./components/Menü/MenuLogikLila";
+import ZurueckIcon from "./components/ZurückButton";
 import './App.css';
 
 export default function NotFound() {
+    const items =[
+        { text: 'Home', link: '/start' },
+        { text: 'Account', },
+        { text: 'Kategorien',},
+        { text: 'Anfrage stellen', link: '/anfrage' },
+        { text: 'Meine Chats', },
+        ];
+
     return (
         <div>
+            <MenuIcon items={items} />
+            <Link to='/start'>
+                <ZurueckIcon/>
+            </Link>
             <TitleBar />
             <h1 className="errorüberschrift">404<br /> Ups, diese Seite existiert noch nicht</h1>
             
