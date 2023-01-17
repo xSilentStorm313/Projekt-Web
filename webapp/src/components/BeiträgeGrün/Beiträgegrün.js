bcc691d3555d825e85da6102f34d49e07f4432cb
Katharina Grünwald <kgruenwa@hm.edu>
2023-01-17 22:55:53 +0100

    HAUptseit egrün erfahrungsbereich

--- webapp/src/components/BeiträgeGrün/Beiträgegrün.js
+++ webapp/src/components/BeiträgeGrün/Beiträgegrün.js
@@ -2,15 +2,16 @@ import React from "react";
 import rückkehr from "./BilderbeiträgeGrün/Rückkehr.png";
 import verkehr from "./BilderbeiträgeGrün/verkehr.png";
 
-export default function Button() {
+export default function Beiträgegrün() {
   return (
     <div className="abstand">
+      <button className="buttonerfahrungsbereich">Erfahrungsbereich</button>
  
       <a href="/verkehr"><span className="buttonbeitraggrün">
         <img src={verkehr} className="formular" alt="formular" />Verkehr <span className="beschreibung"> <br />Hier findest du Informationen zum Thema kostenfeier Verkehr mit den Verkehrs- mitteln für ukrainische Geflüchtete</span></span></a>
       
-        <a href="/rückkehr"><span className="buttonbeitraggrün">
-        <img src={rückkehr} className="formular" alt="formular" />freiwillige Rückkehr <span className="beschreibung"> <br />Hier findest du Informationen zum Thema freiwillige Rückkehr als Geflüchteter in das Ursprungsland</span></span></a>
+      <a href="/rückkehr"><span className="buttonbeitraggrün">
+      <img src={rückkehr} className="formular" alt="formular" />freiwillige Rückkehr <span className="beschreibung"> <br />Hier findest du Informationen zum Thema freiwillige Rückkehr als Geflüchteter in das Ursprungsland</span></span></a>
 
     </div>
   );
