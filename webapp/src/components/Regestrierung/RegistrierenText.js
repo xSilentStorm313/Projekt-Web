0ec2590b3a9fc971d15beb163020b00eb228b7a6
Katharina Grünwald <kgruenwa@hm.edu>
2023-01-18 10:28:05 +0100

    Regestrieren

--- webapp/src/components/Regestrierung/RegistrierenText.js
+++ webapp/src/components/Regestrierung/RegistrierenText.js
@@ -149,10 +149,10 @@ const RegestrierungText = () => {
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
-        <option value="3">4</option>
-        <option value="3">5</option>
-        <option value="3">6</option>
-        <option value="3">Mehr als 6</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="Mehr als 6">Mehr als 6</option>
       </select>
 
       <p className="registrierungsbeschreibung">Herkunftsland</p>
