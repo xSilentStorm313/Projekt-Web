791ab1a3de058e8aa0d0bbed6ca002b5667a7707
Amir Al Nahawandi <amir.al@hm.edu>
2023-01-19 21:55:54 +0100

    kommentar nur möglich mit inhalt

--- webapp/src/components/Kommentare/KommentarForm.js
+++ webapp/src/components/Kommentare/KommentarForm.js
@@ -6,14 +6,17 @@ function CommentForm({ onSubmit }) {
   return (
     <form onSubmit={e => {
       e.preventDefault();
-      onSubmit({ text, id: Date.now() });
-      setText('');
+      if (text.trim()) {
+        onSubmit({ text, id: Date.now() });
+        setText('');
+      }
     }} className="form-container">
         <input type="text" name="Kommentar" className="kommentarInput" placeholder="Kommentar hinzufügen..." value={text} onChange={e => setText(e.target.value)}/>
       <br />
-        <button type="submit" className="submit-button">Senden</button>
+        <button type="submit" className={`submit-button`} disabled={!text.trim()}>Senden</button>
     </form>
   );
+
 }
 
 export default CommentForm;
