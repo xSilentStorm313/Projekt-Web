3af6e5163f673c5ea6895bcd7e2a647491d2e745
Amir Al Nahawandi <amir.al@hm.edu>
2023-01-20 12:42:45 +0100

    link to fixed

--- webapp/src/components/BeiträgeGrün/Beiträgegrün.js
+++ webapp/src/components/BeiträgeGrün/Beiträgegrün.js
@@ -2,6 +2,7 @@ import React from "react";
 import rückkehr from "./BilderbeiträgeGrün/Rückkehr.png";
 import verkehr from "./BilderbeiträgeGrün/verkehr.png";
 import { AiOutlineRight } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 export default function Beiträgegrün() {
   return (
@@ -13,11 +14,15 @@ export default function Beiträgegrün() {
       </div>
       </button>
 
-      <a href="/#/verkehr"><span className="buttonbeitraggrün">
-        <img src={verkehr} className="formular" alt="formular" /><span className="überschriften-hauptseite">Verkehr</span><span className="beschreibung"> <br />Hier findest du Informationen zum Thema freiwillige Rückkehr als Geflüchteter</span></span></a>
+      <Link to="/verkehr">
+      <span className="buttonbeitraggrün">
+        <img src={verkehr} className="formular" alt="formular" /><span className="überschriften-hauptseite">Verkehr</span><span className="beschreibung"> <br />Hier findest du Informationen zum Thema freiwillige Rückkehr als Geflüchteter</span></span>
+      </Link>
 
-      <a href="/#/rückkehr"><span className="buttonbeitraggrün-rückkehr">
-        <img src={rückkehr} className="formular" alt="formular" /><span className="überschriften-hauptseite">Freiwillige Rückkehr</span><span className="beschreibung"> <br />Hier findest du Informationen zum Thema kostenfreier Verkehr für ukrainische Geflüchtete</span></span></a>
+      <Link to="/rückkehr">
+      <span className="buttonbeitraggrün-rückkehr">
+        <img src={rückkehr} className="formular" alt="formular" /><span className="überschriften-hauptseite">Freiwillige Rückkehr</span><span className="beschreibung"> <br />Hier findest du Informationen zum Thema kostenfreier Verkehr für ukrainische Geflüchtete</span></span>
+        </Link>
 
     </div>
   );
--- webapp/src/components/BeiträgeLila/Beiträgelila.js
+++ webapp/src/components/BeiträgeLila/Beiträgelila.js
@@ -26,27 +26,42 @@ export default function Button() {
           wie du als EU und non-EU Bürger in Deutschland arbeiten kannst</span> </span>
       </Link>
 
-      <a href="/#/anerkennungabschluesse"><span className="buttonbeitrag">
-        <img src={anerkennung} className="formular" alt="formular" /><span className="überschriften-hauptseite">Abschluss Anerkennung</span><span className="beschreibung"> <br />Hier wird dir gezeigt, wie du welche Abschlüsse anerkennen lassen kannst</span></span></a>
+      <Link to="/anerkennungabschluesse">
+      <span className="buttonbeitrag">
+        <img src={anerkennung} className="formular" alt="formular" /><span className="überschriften-hauptseite">Abschluss Anerkennung</span><span className="beschreibung"> <br />Hier wird dir gezeigt, wie du welche Abschlüsse anerkennen lassen kannst</span></span>
+        </Link>
 
-      <a href="/#/asylantrag"><span className="buttonbeitrag">
+        <Link to="/asylantrag">
+        <span className="buttonbeitrag">
         <img src={asylbeantragen} className="formular" alt="formular" /><span className="überschriften-hauptseite">Asyl Beantragung</span>
-        <span className="beschreibung-asyl-verein"> <br />Hier erhälst du Hilfe für dein Asylverfahren</span> </span> </a>
+        <span className="beschreibung-asyl-verein"> <br />Hier erhälst du Hilfe für dein Asylverfahren</span> </span> 
+        </Link>
 
-      <a href="/#/vereinsanmeldung"><span className="buttonbeitrag">
-        <img src={sportverein} className="formular" alt="formular" /><span className="überschriften-hauptseite">Vereinsanmeldung</span><span className="beschreibung-asyl-verein"> <br />Hier findest Sport und Freizeitangebote</span></span></a>
+        <Link to="/vereinsanmeldung">
+        <span className="buttonbeitrag">
+        <img src={sportverein} className="formular" alt="formular" /><span className="überschriften-hauptseite">Vereinsanmeldung</span><span className="beschreibung-asyl-verein"> <br />Hier findest Sport und Freizeitangebote</span></span>
+        </Link>
 
-      <a href="/#/wohngeld"><span className="buttonbeitrag">
-        <img src={wohnen} className="formular" alt="formular" /><span className="überschriften-hauptseite">Wohngeld Beantragung</span><span className="beschreibung"> <br />Hier findest du Informationen zum Thema Wohnen</span></span></a>
+        <Link to="/wohngeld">
+        <span className="buttonbeitrag">
+        <img src={wohnen} className="formular" alt="formular" /><span className="überschriften-hauptseite">Wohngeld Beantragung</span><span className="beschreibung"> <br />Hier findest du Informationen zum Thema Wohnen</span></span>
+        </Link>
 
-      <a href="/#/kindergeld"><span className="buttonbeitrag">
-        <img src={formular} className="formular-kindergeld" alt="formular" /> <br /> <br /><span className="überschriften-hauptseite">Kindergeld</span><span className="beschreibung-kindergeld"> <br />Ich hätte eine Frage zu Absatz 3, woher kriege ich folgende Informationen?</span> </span></a>
+        <Link to="/kindergeld">
+        <span className="buttonbeitrag">
+        <img src={formular} className="formular-kindergeld" alt="formular" /> <br /> <br /><span className="überschriften-hauptseite">Kindergeld</span><span className="beschreibung-kindergeld"> <br />Ich hätte eine Frage zu Absatz 3, woher kriege ich folgende Informationen?</span> </span>
+        </Link>
 
-      <a href="/#/rückkehrlila"><span className="buttonbeitrag">
-        <img src={rückkehr} className="formular" alt="formular" /><span className="überschriften-hauptseite">Freiwillige Rückkehr</span><span className="beschreibung"> <br />Hier findest du Informationen zum Thema freiwillige Rückkehr als Geflüchteter</span></span></a>
+        <Link to="/rückkehrlila">
+      <span className="buttonbeitrag">
+        <img src={rückkehr} className="formular" alt="formular" /><span className="überschriften-hauptseite">Freiwillige Rückkehr</span><span className="beschreibung"> <br />Hier findest du Informationen zum Thema freiwillige Rückkehr als Geflüchteter</span></span>
+        </Link>
 
-      <a href="/#/verkehrlila"><span className="buttonbeitrag-verkehr">
-        <img src={verkehr} className="formular" alt="formular" /><span className="überschriften-hauptseite">Verkehr</span><span className="beschreibung"> <br />Hier findest du Informationen zum Thema kostenfreier Verkehr für ukrainische Geflüchtete</span></span></a>
+        <Link to="/verkehrlila">
+        <span className="buttonbeitrag-verkehr">
+        <img src={verkehr} className="formular" alt="formular" /><span className="überschriften-hauptseite">Verkehr</span><span className="beschreibung"> <br />Hier findest du Informationen zum Thema kostenfreier Verkehr für ukrainische Geflüchtete</span></span>
+        </Link>
     </div>
+
   );
 }
