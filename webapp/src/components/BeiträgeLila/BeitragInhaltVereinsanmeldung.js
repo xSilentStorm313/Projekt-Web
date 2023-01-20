560c21845a18f28de01f7570890e24838b73549a
Katharina Grünwald <kgruenwa@hm.edu>
2023-01-20 12:49:23 +0100

    vereinsanmeldung

--- webapp/src/components/BeiträgeLila/BeitragInhaltVereinsanmeldung.js
+++ webapp/src/components/BeiträgeLila/BeitragInhaltVereinsanmeldung.js
@@ -9,10 +9,14 @@ export default function BeitragInhaltVereinsanmeldung() {
             <img src={sportverein} className="formular" alt="formular" />
             <button className="downloadbutton">Formulare herunterladen</button>
             <h1 className="überschrift1">Vereinsanmeldung</h1>
-            <p className="text">In Deutschland kommt es bei der Anerkennung der Abschlüsse sehr auf den jeweiligen Abschluss an.</p>
-            <div className="infotextasyl">Dementsprechend empfehlen wir Ihnen das <a href="https://www.anerkennung-in-deutschland.de/html/de/fachkraefte.php">Infoportal der Bundesregierung
-                zur Anerkennung ausländischer Berufsqualifikationen</a> anzuschauen.</div>
-            <p className="text">Geben Sie in die Suchleiste Ihren Job ein, den Sie in Ihrem Land ausgeführt haben. Und das Portal führt Sie durch die einzelnen Schritte, die Sie brauchen, um Ihre Abschlüsse und Ihre Arbeitserfahrung in Deutschland anerkennen lassen zu können.</p>
+            <div className="infotextasyl">Anmeldung in einem Verein in Deutschland ist in der Regel ein einfacher Prozess.</div>
+            <p className="text">Hier sind die grundlegenden Schritte, die Sie befolgen sollten:</p>
+            <p className="text">1. Suchen Sie sich den richtigen Verein aus: Recherchieren Sie nach Vereinen, die Ihren Interessen entsprechen und überprüfen Sie deren Ziele und Aktivitäten.</p>
+            <p className="text">2. Kontaktieren Sie den Verein: Sie können den Verein telefonisch, per E-Mail oder über die sozialen Medien kontaktieren, um weitere Informationen über die Anmeldung zu erhalten.</p>
+            <p className="text">3. Ausfüllen des Anmeldeformulars: Der Verein wird Ihnen ein Anmeldeformular zur Verfügung stellen, das Sie ausfüllen und zurücksenden müssen. Stellen Sie sicher, dass alle erforderlichen Informationen korrekt angegeben werden.</p>
+            <p className="text">4. Bezahlen der Mitgliedsgebühren: Sobald der Verein Ihre Anmeldung bestätigt hat, müssen Sie die Mitgliedsgebühren bezahlen. Die Höhe der Gebühren variiert je nach Verein.</p>
+            <p className="text">5. Willkommen im Verein: Sobald Sie alle Schritte abgeschlossen haben, werden Sie offiziell Mitglied des Vereins und können an seinen Aktivitäten teilnehmen.</p>
+            <div className="infotextasyl">Hinweis: Einige Vereine haben spezielle Anforderungen, bevor man Mitglied werden kann, wie z.B. eine Probezeit oder eine Aufnahmeprüfung, deshalb ist es wichtig, sich vorher genau über die Anmeldungsbedingungen zu informieren.</div>
 
 
 
